3d065226b27ca9b4531f398fbe25e4f4091782ea
wenj <j_1wen@163.com>
2025-10-18 20:32:52 -0500

    pages

---
+++ files/select.js
@@ -0,0 +1,222 @@
+var freeviewThumanVideos = [
+    ["0019_gt.mp4", "0019_lifegom_gt.mp4", "0019_lifegom_multihmr.mp4", "0019_ours.mp4"],
+    ["0042_gt.mp4", "0042_lifegom_gt.mp4", "0042_lifegom_multihmr.mp4", "0042_ours.mp4"],
+    ["0108_gt.mp4", "0108_lifegom_gt.mp4", "0108_lifegom_multihmr.mp4", "0108_ours.mp4"],
+    ["0156_gt.mp4", "0156_lifegom_gt.mp4", "0156_lifegom_multihmr.mp4", "0156_ours.mp4"],
+    ["0162_gt.mp4", "0162_lifegom_gt.mp4", "0162_lifegom_multihmr.mp4", "0162_ours.mp4"],
+    ["0252_gt.mp4", "0252_lifegom_gt.mp4", "0252_lifegom_multihmr.mp4", "0252_ours.mp4"],
+    ["0402_gt.mp4", "0402_lifegom_gt.mp4", "0402_lifegom_multihmr.mp4", "0402_ours.mp4"]
+]
+
+var freeviewHuge100kVideos = [
+    ["deepfashion_images0_A_pose_MEN-Sweaters-id_00000702-07_7_additional_gt.mp4",
+     "deepfashion_images0_A_pose_MEN-Sweaters-id_00000702-07_7_additional_idol.mp4",
+     "deepfashion_images0_A_pose_MEN-Sweaters-id_00000702-07_7_additional_lhm.mp4",
+     "deepfashion_images0_A_pose_MEN-Sweaters-id_00000702-07_7_additional_ours.mp4"],
+
+    ["deepfashion_images0_A_pose_WOMEN-Dresses-id_00002093-02_7_additional_gt.mp4",
+     "deepfashion_images0_A_pose_WOMEN-Dresses-id_00002093-02_7_additional_idol.mp4",
+     "deepfashion_images0_A_pose_WOMEN-Dresses-id_00002093-02_7_additional_lhm.mp4",
+     "deepfashion_images0_A_pose_WOMEN-Dresses-id_00002093-02_7_additional_ours.mp4"],
+
+    ["deepfashion_images0_A_pose_WOMEN-Dresses-id_00003332-08_4_full_gt.mp4",
+     "deepfashion_images0_A_pose_WOMEN-Dresses-id_00003332-08_4_full_idol.mp4",
+     "deepfashion_images0_A_pose_WOMEN-Dresses-id_00003332-08_4_full_lhm.mp4",
+     "deepfashion_images0_A_pose_WOMEN-Dresses-id_00003332-08_4_full_ours.mp4"],
+
+    ["deepfashion_images0_A_pose_WOMEN-Dresses-id_00005264-01_1_front_gt.mp4",
+     "deepfashion_images0_A_pose_WOMEN-Dresses-id_00005264-01_1_front_idol.mp4",
+     "deepfashion_images0_A_pose_WOMEN-Dresses-id_00005264-01_1_front_lhm.mp4",
+     "deepfashion_images0_A_pose_WOMEN-Dresses-id_00005264-01_1_front_ours.mp4"],
+
+    ["flux_batch4_images7_Chile_female_slight_sport shirts_40~50 years old_654_gt.mp4",
+     "flux_batch4_images7_Chile_female_slight_sport shirts_40~50 years old_654_idol.mp4",
+     "flux_batch4_images7_Chile_female_slight_sport shirts_40~50 years old_654_lhm.mp4",
+     "flux_batch4_images7_Chile_female_slight_sport shirts_40~50 years old_654_ours.mp4"],
+
+    ["flux_batch6_images7_Pakistan_female_built_gloves_50~60 years old_999_gt.mp4",
+     "flux_batch6_images7_Pakistan_female_built_gloves_50~60 years old_999_idol.mp4",
+     "flux_batch6_images7_Pakistan_female_built_gloves_50~60 years old_999_lhm.mp4",
+     "flux_batch6_images7_Pakistan_female_built_gloves_50~60 years old_999_ours.mp4"],
+
+    ["flux_batch6_images7_United Arab Emirates_female_large_regalia_50~60 years old_1741_gt.mp4",
+     "flux_batch6_images7_United Arab Emirates_female_large_regalia_50~60 years old_1741_idol.mp4",
+     "flux_batch6_images7_United Arab Emirates_female_large_regalia_50~60 years old_1741_lhm.mp4",
+     "flux_batch6_images7_United Arab Emirates_female_large_regalia_50~60 years old_1741_ours.mp4"],
+
+    ["flux_batch8_images9_Puerto Rico_female_athletic_clerical vestments_20~30 years old_1079_gt.mp4",
+     "flux_batch8_images9_Puerto Rico_female_athletic_clerical vestments_20~30 years old_1079_idol.mp4",
+     "flux_batch8_images9_Puerto Rico_female_athletic_clerical vestments_20~30 years old_1079_lhm.mp4",
+     "flux_batch8_images9_Puerto Rico_female_athletic_clerical vestments_20~30 years old_1079_ours.mp4"],
+
+    ["flux_batch9_images7_Solomon Islands_female_buff_gloves_20~30 years old_1566_gt.mp4",
+     "flux_batch9_images7_Solomon Islands_female_buff_gloves_20~30 years old_1566_idol.mp4",
+     "flux_batch9_images7_Solomon Islands_female_buff_gloves_20~30 years old_1566_lhm.mp4",
+     "flux_batch9_images7_Solomon Islands_female_buff_gloves_20~30 years old_1566_ours.mp4"]
+]
+
+var npsThumanVideos = [
+    ["thuman2.0_mimo2/0000.png", "thuman2.0_mimo2/mimo2_fps20.mp4", "thuman2.0_mimo2/0000_novel_pose.mp4"],
+    ["thuman2.0_mimo2/0019.png", "thuman2.0_mimo2/mimo2_fps20.mp4", "thuman2.0_mimo2/0019_novel_pose.mp4"],
+    ["thuman2.0_mimo2/0456.png", "thuman2.0_mimo2/mimo2_fps20.mp4", "thuman2.0_mimo2/0456_novel_pose.mp4"],
+
+    ["thuman2.0_mimo6/0001.png", "thuman2.0_mimo6/mimo6_fps20.mp4", "thuman2.0_mimo6/0001_novel_pose.mp4"],
+    ["thuman2.0_mimo6/0018.png", "thuman2.0_mimo6/mimo6_fps20.mp4", "thuman2.0_mimo6/0018_novel_pose.mp4"],
+    ["thuman2.0_mimo6/0108.png", "thuman2.0_mimo6/mimo6_fps20.mp4", "thuman2.0_mimo6/0108_novel_pose.mp4"],
+
+    ["thuman2.0_taiji/0084.png", "thuman2.0_taiji/taiji_fps20.mp4", "thuman2.0_taiji/0084_novel_pose.mp4"],
+    ["thuman2.0_taiji/0168.png", "thuman2.0_taiji/taiji_fps20.mp4", "thuman2.0_taiji/0168_novel_pose.mp4"],
+    ["thuman2.0_taiji/0402.png", "thuman2.0_taiji/taiji_fps20.mp4", "thuman2.0_taiji/0402_novel_pose.mp4"]
+]
+
+var npsHuge100kVideos = [
+    ["huge100k_mimo1/flux_batch3_images7_China_female_stocky_thermal leggings_60~70 years old_503.png", "huge100k_mimo1/mimo1_fps20.mp4", "huge100k_mimo1/flux_batch3_images7_China_female_stocky_thermal leggings_60~70 years old_503_novel_pose.mp4"],
+    ["huge100k_mimo1/flux_batch3_images7_Japan_male_bodybuilder_chiffon blouses_70~80 years old_12.png", "huge100k_mimo1/mimo1_fps20.mp4", "huge100k_mimo1/flux_batch3_images7_Japan_male_bodybuilder_chiffon blouses_70~80 years old_12_novel_pose.mp4"],
+    ["huge100k_mimo1/flux_batch4_images7_Argentina_male_petite_rain boots_70~80 years old_188.png", "huge100k_mimo1/mimo1_fps20.mp4", "huge100k_mimo1/flux_batch4_images7_Argentina_male_petite_rain boots_70~80 years old_188_novel_pose.mp4"],
+    ["huge100k_mimo1/flux_batch4_images7_Brazil_female_large_eco-friendly clothing_80~90 years old_1458.png", "huge100k_mimo1/mimo1_fps20.mp4", "huge100k_mimo1/flux_batch4_images7_Brazil_female_large_eco-friendly clothing_80~90 years old_1458_novel_pose.mp4"],
+    ["huge100k_mimo1/flux_batch4_images7_Colombia_male_built_track pants_70~80 years old_17.png", "huge100k_mimo1/mimo1_fps20.mp4", "huge100k_mimo1/flux_batch4_images7_Colombia_male_built_track pants_70~80 years old_17_novel_pose.mp4"],
+    ["huge100k_mimo1/flux_batch4_images7_Morocco_female_bodybuilder_chiffon blouses_70~80 years old_1562.png", "huge100k_mimo1/mimo1_fps20.mp4", "huge100k_mimo1/flux_batch4_images7_Morocco_female_bodybuilder_chiffon blouses_70~80 years old_1562_novel_pose.mp4"],
+    ["huge100k_mimo1/flux_batch4_images7_Puerto_Rico_female_lean_scarves_80~90 years old_1514.png", "huge100k_mimo1/mimo1_fps20.mp4", "huge100k_mimo1/flux_batch4_images7_Puerto_Rico_female_lean_scarves_80~90 years old_1514_novel_pose.mp4"],
+    ["huge100k_mimo1/flux_batch4_images7_Turkey_female_lean_linen dresses_20~30 years old_342.png", "huge100k_mimo1/mimo1_fps20.mp4", "huge100k_mimo1/flux_batch4_images7_Turkey_female_lean_linen dresses_20~30 years old_342_novel_pose.mp4"],
+    ["huge100k_mimo1/flux_batch5_images9_Belize_male_large_scarves_60~70 years old_518.png", "huge100k_mimo1/mimo1_fps20.mp4", "huge100k_mimo1/flux_batch5_images9_Belize_male_large_scarves_60~70 years old_518_novel_pose.mp4"],
+
+    ["huge100k_mimo4/deepfashion_images0_A_pose_WOMEN-Blouses_Shirts-id_00005793-01_4_full.png", "huge100k_mimo4/mimo4_fps20.mp4", "huge100k_mimo4/deepfashion_images0_A_pose_WOMEN-Blouses_Shirts-id_00005793-01_4_full_novel_pose.mp4"],
+    ["huge100k_mimo4/deepfashion_images0_A_pose_WOMEN-Denim-id_00005157-01_1_front.png", "huge100k_mimo4/mimo4_fps20.mp4", "huge100k_mimo4/deepfashion_images0_A_pose_WOMEN-Denim-id_00005157-01_1_front_novel_pose.mp4"],
+    ["huge100k_mimo4/deepfashion_images0_A_pose_WOMEN-Jackets_Coats-id_00001935-02_7_additional.png", "huge100k_mimo4/mimo4_fps20.mp4", "huge100k_mimo4/deepfashion_images0_A_pose_WOMEN-Jackets_Coats-id_00001935-02_7_additional_novel_pose.mp4"],
+    ["huge100k_mimo4/deepfashion_images0_A_pose_WOMEN-Sweaters-id_00005253-04_1_front.png", "huge100k_mimo4/mimo4_fps20.mp4", "huge100k_mimo4/deepfashion_images0_A_pose_WOMEN-Sweaters-id_00005253-04_1_front_novel_pose.mp4"],
+    ["huge100k_mimo4/flux_batch1_images7_El Salvador_male_athletic_fur jackets_40~50 years old_845.png", "huge100k_mimo4/mimo4_fps20.mp4", "huge100k_mimo4/flux_batch1_images7_El Salvador_male_athletic_fur jackets_40~50 years old_845_novel_pose.mp4"],
+    ["huge100k_mimo4/flux_batch1_images7_France_female_stocky_cotton shirts_30~40 years old_21.png", "huge100k_mimo4/mimo4_fps20.mp4", "huge100k_mimo4/flux_batch1_images7_France_female_stocky_cotton shirts_30~40 years old_21_novel_pose.mp4"],
+    ["huge100k_mimo4/flux_batch1_images7_Jamaica_male_athletic_fur jackets_50~60 years old_1120.png", "huge100k_mimo4/mimo4_fps20.mp4", "huge100k_mimo4/flux_batch1_images7_Jamaica_male_athletic_fur jackets_50~60 years old_1120_novel_pose.mp4"],
+    ["huge100k_mimo4/flux_batch1_images7_Saudi Arabia_female_stocky_hiking clothes_30~40 years old_1712.png", "huge100k_mimo4/mimo4_fps20.mp4", "huge100k_mimo4/flux_batch1_images7_Saudi Arabia_female_stocky_hiking clothes_30~40 years old_1712_novel_pose.mp4"],
+    ["huge100k_mimo4/flux_batch2_images7_Iran_male_built_lab coats_30~40 years old_1513.png", "huge100k_mimo4/mimo4_fps20.mp4", "huge100k_mimo4/flux_batch2_images7_Iran_male_built_lab coats_30~40 years old_1513_novel_pose.mp4"]
+]
+
+var crossdomainPeoplesnapshotVideos = [
+    ["f3c.png", "f3c_freeview.mp4"],
+    ["f4c.png", "f4c_freeview.mp4"],
+    ["m3c.png", "m3c_freeview.mp4"],
+    ["m4c.png", "m4c_freeview.mp4"],
+]
+
+var crossdomainUbcVideos = [
+    ["91+xeI+ijRS.png", "91+xeI+ijRS_freeview.mp4"],
+    ["917v6EQuPJS.png", "917v6EQuPJS_freeview.mp4"],
+    ["91c+SL7Cg7S.png", "91c+SL7Cg7S_freeview.mp4"],
+    ["91WvLcNpdzS.png", "91WvLcNpdzS_freeview.mp4"],
+]
+
+function ChangeSceneFreeviewThuman(idx){
+    var li_list = document.getElementById("freeview_thuman").children;
+    // var m_list = document.getElementById("method-view-ul").children;
+    // console.log(idx);
+    // console.log(li_list);
+
+    for(i = 0; i < li_list.length; i++){
+        if (li_list[i].className === "disabled"){
+            continue
+        }
+        li_list[i].className = "";
+    }
+    li_list[idx].className = "active";
+
+    currentVideos = freeviewThumanVideos[idx]
+    document.getElementById("freeview_thuman_gt").src = "./medias/comparison_thuman2.0/" + currentVideos[0];
+    document.getElementById("freeview_thuman_lifegom_gt").src = "./medias/comparison_thuman2.0/" + currentVideos[1];
+    document.getElementById("freeview_thuman_lifegom_multihmr").src = "./medias/comparison_thuman2.0/" + currentVideos[2];
+    document.getElementById("freeview_thuman_ours").src = "./medias/comparison_thuman2.0/" + currentVideos[3];
+}
+
+function ChangeSceneFreeviewHuge100k(idx){
+    var li_list = document.getElementById("freeview_huge100k").children;
+    // var m_list = document.getElementById("method-view-ul").children;
+    // console.log(idx);
+    // console.log(li_list);
+
+    for(i = 0; i < li_list.length; i++){
+        if (li_list[i].className === "disabled"){
+            continue
+        }
+        li_list[i].className = "";
+    }
+    li_list[idx].className = "active";
+
+    currentVideos = freeviewHuge100kVideos[idx]
+    document.getElementById("freeview_huge100k_gt").src = "./medias/comparison_huge100k/" + currentVideos[0];
+    document.getElementById("freeview_huge100k_idol").src = "./medias/comparison_huge100k/" + currentVideos[1];
+    document.getElementById("freeview_huge100k_lhm").src = "./medias/comparison_huge100k/" + currentVideos[2];
+    document.getElementById("freeview_huge100k_ours").src = "./medias/comparison_huge100k/" + currentVideos[3];
+}
+
+function ChangeSceneNpsThuman(idx){
+    var li_list = document.getElementById("nps_thuman").children;
+    // var m_list = document.getElementById("method-view-ul").children;
+    // console.log(idx);
+    // console.log(li_list);
+
+    for(i = 0; i < li_list.length; i++){
+        if (li_list[i].className === "disabled"){
+            continue
+        }
+        li_list[i].className = "";
+    }
+    li_list[idx].className = "active";
+
+    currentVideos = npsThumanVideos[idx]
+    document.getElementById("nps_thuman_ref").src = "./medias/novelpose/" + currentVideos[0];
+    document.getElementById("nps_thuman_pose").src = "./medias/novelpose/" + currentVideos[1];
+    document.getElementById("nps_thuman_video").src = "./medias/novelpose/" + currentVideos[2];
+}
+
+function ChangeSceneNpsHuge100k(idx){
+    var li_list = document.getElementById("nps_huge100k").children;
+    // var m_list = document.getElementById("method-view-ul").children;
+    // console.log(idx);
+    // console.log(li_list);
+
+    for(i = 0; i < li_list.length; i++){
+        if (li_list[i].className === "disabled"){
+            continue
+        }
+        li_list[i].className = "";
+    }
+    if (idx < 9) {
+        li_list[idx].className = "active";
+    } else {
+        li_list[idx + 1].className = "active";
+    }
+
+    currentVideos = npsHuge100kVideos[idx]
+    document.getElementById("nps_huge100k_ref").src = "./medias/novelpose/" + currentVideos[0];
+    document.getElementById("nps_huge100k_pose").src = "./medias/novelpose/" + currentVideos[1];
+    document.getElementById("nps_huge100k_video").src = "./medias/novelpose/" + currentVideos[2];
+}
+
+function ChangeSceneCrossDomainPeoplesnapshot(idx){
+    var li_list = document.getElementById("crossdomain_peoplesnapshot").children;
+    for(i = 0; i < li_list.length; i++){
+        if (li_list[i].className === "disabled"){
+            continue
+        }
+        li_list[i].className = "";
+    }
+    li_list[idx].className = "active";
+    
+    currentVideos = crossdomainPeoplesnapshotVideos[idx]
+    document.getElementById("crossdomain_peoplesnapshot_ref").src = "./medias/crossdomain/" + currentVideos[0];
+    document.getElementById("crossdomain_peoplesnapshot_video").src = "./medias/crossdomain/" + currentVideos[1];
+}
+
+function ChangeSceneCrossDomainUbc(idx){
+    var li_list = document.getElementById("crossdomain_ubc").children;
+    for(i = 0; i < li_list.length; i++){
+        if (li_list[i].className === "disabled"){
+            continue
+        }
+        li_list[i].className = "";
+    }
+    li_list[idx].className = "active";
+    
+    currentVideos = crossdomainUbcVideos[idx]
+    document.getElementById("crossdomain_ubc_ref").src = "./medias/crossdomain/" + currentVideos[0];
+    document.getElementById("crossdomain_ubc_video").src = "./medias/crossdomain/" + currentVideos[1];
+}
